c750df118d0e8c6302cdae74772dd213d1893699
Michael Huynh <michaelthuynh@gmail.com>
2019-08-05 09:38:04 -0700

    Updates

---
+++ precache-manifest.b0da9275753467781586f26ace7cc888.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "206703002b74970951dce9fa01301dcd",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "5793876d1eea75744992",
+    "url": "/todo/static/css/main.4f2b9848.chunk.css"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/todo/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "5793876d1eea75744992",
+    "url": "/todo/static/js/main.f2c35fb7.chunk.js"
+  },
+  {
+    "revision": "3ce2d46cb5510c372f4d",
+    "url": "/todo/static/js/runtime~main.7a2dc5ec.js"
+  }
+]);
